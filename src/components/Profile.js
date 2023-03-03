227bd14cb7888c7d365189d93d20d61b49bd51ac
Josh Grainge <josh.grainge@hotmail.com>
2023-03-03 16:32:15 -0500

    Updated Profile for stylings

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,13 +2,19 @@ export default function Profile() {
   return (
     <>
       <div className="profile-pic"></div>
-      <h1>Josh Grainge</h1>
-      <p>Frontend Developer</p>
-      <a href="github.com/JoshGrainge">
-        <small>GitHub</small>
+      <h1 className="name-title">Josh Grainge</h1>
+      <p className="job-title">Frontend Developer</p>
+      <a href="https://github.com/JoshGrainge" className="website-link">
+        Website
       </a>
-      <button>Email</button>
-      <button>Linkedin</button>
+      <div className="button-container">
+        <button className="white">
+          <i className="fa fa-envelope"></i>Email
+        </button>
+        <button className="blue">
+          <i className="fa fa-linkedin-square"></i>Linkedin
+        </button>
+      </div>
     </>
   );
 }
